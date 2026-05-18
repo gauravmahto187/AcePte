4c0ea9980824039c5317c4c5fce111f9dc19ff98
Gauravmahto187 <gauravmahto269@gmail.com>
2026-05-18 18:41:48 +0545

    Jume 1

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
         "High-quality templates and practical resources for each section.",
         "Classes run Monday to Friday for 30 days."
       ],
-      nextSession: "6:00 – 7:00 PM (23rd March)",
+      nextSession: "6:00 – 7:00 PM (1st June)",
       sessionType: "Full Course In-Depth"
     },
     
